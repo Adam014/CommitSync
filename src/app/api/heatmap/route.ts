89ed057b11e7238e7ab571717a9b199c47416254
Adam Stádník <adam.stadnik@displio.eu>
2025-04-14 20:38:12 +0200

    fixing build

--- src/app/api/heatmap/route.ts
+++ src/app/api/heatmap/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { format } from "date-fns";
 import { getColor } from "@/stores/utils"; // Reuse your existing getColor logic
+import { GitHubCommit, GitLabCommit } from "@/types/types";
 
 // Legend thresholds used for the legend squares (customize if needed)
 const legendCounts = [0, 1, 3, 6, 10];
@@ -37,7 +38,7 @@ async function fetchEvents(
         }
       );
       const ghData = await ghRes.json();
-      (ghData.items || []).forEach((commit: any) => {
+      (ghData.items || []).forEach((commit: GitHubCommit) => {
         const commitDate = new Date(commit.commit.author.date);
         const dateKey = format(commitDate, "yyyy-MM-dd");
         if (dateKey in eventsAggregate) {
@@ -67,7 +68,7 @@ async function fetchEvents(
           { headers: { "Private-Token": gitlabToken } }
         );
         const glEvents = await glEventsRes.json();
-        (glEvents || []).forEach((event: any) => {
+        (glEvents || []).forEach((event: GitLabCommit) => {
           const eventDate = new Date(event.created_at);
           if (
             eventDate.getFullYear() === currentYear &&
